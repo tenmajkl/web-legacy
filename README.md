ad46b4ca1c5fc676029ab9464a665664f05e2236
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-01-22 23:36:18 +0100

    revert

---
+++ README.md
@@ -0,0 +1,11 @@
+# Web
+
+Personal website writen in vue
+
+## Installation
+
+```sh
+$ git clone https://github.com/TENMAJKL/web
+$ npm i
+$ npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <terminal></terminal>
+    </div>
+</template>
+
+<script>
+import Terminal from "./components/Terminal.vue"
+
+export default {
+    components: {
+        Terminal
+    }
+}
+</script>
+
