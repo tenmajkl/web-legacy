0fee3bd5d45d17a6dc61761728fe06ea0f95880b
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-01-23 11:54:13 +0100

    removes auto deployment

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/TENMAJKL/TENMAJKL.github.io master
+git push -f git@github.com:TENMAJKL/TENMAJKL.github.io.git master
 
 cd -
